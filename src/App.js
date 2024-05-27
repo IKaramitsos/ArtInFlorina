85dc764e87c931983512d4d5368508fe4e5e5901
IKaramitsos <ioakaras@yahoo.com>
2024-05-27 12:31:57 +0300

    Fix url problem

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Event from "./pages/Event";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import EventDetail from './components/EventDetail';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 
 function App() {
   return (
@@ -14,6 +14,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
+        <Route path="/ArtInFlorina" element={<Navigate to="/" />} /> 
           <Route exact path="/" element={<Home />} />
           <Route exact path="/event" element={<Event />} />
           <Route exact path="/about" element={<About />} />
